7bcebc3f1e1849b46bb484471ff223948e3b38a6
Kevin <mingeun.k.k@gmail.com>
2019-02-17 02:11:35 +0900

    Added user relevant working finish

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -1,6 +1,10 @@
 <?php
 namespace App\Http\Controllers;
 use Illuminate\Http\Request;
+use App\Http\Requests\RegisterFormRequest;
+
+use App\User;
+
 class UserController extends Controller {
     /**
      * Display a listing of the resource.
@@ -8,26 +12,55 @@ class UserController extends Controller {
      * @return \Illuminate\Http\Response
      */
     public function index() {
-        //
+        return response()->json(User::scopeAllUsers());
     }
+
     /**
      * Store a newly created resource in storage.
+     *
+     * @param RegisterFormRequest $request
+     * @return \Illuminate\Contracts\Routing\ResponseFactory|\Illuminate\Http\Response
+     */
+
+    public function store(RegisterFormRequest $request) {
+        $user = new User;
+        $user->email = $request->email;
+        $user->name = $request->name;
+        $user->password = bcrypt($request->password);
+        $user->save();
+
+        if ($user) {
+            return response()->json([
+                'status' => 'success',
+                'data' => $user
+            ], 201);
+        } else {
+            return response()->json([
+                'error' => 'test'
+            ], 400);
+        }
+    }
+
+    /**
+     * Store a newly created discord account
      *
      * @param  \Illuminate\Http\Request $request
      * @return \Illuminate\Http\Response
      */
-    public function store(Request $request) {
-        //
+    public function connectDiscord(Request $request) {
+
     }
+
     /**
      * Display the specified resource.
      *
      * @param  int $id
      * @return \Illuminate\Http\Response
      */
-    public function show($id) {
-        //
+    public function show(int $id) {
+        return response()->json(User::scopeGetUser($id));
     }
+
     /**
      * Update the specified resource in storage.
      *
@@ -35,16 +68,17 @@ class UserController extends Controller {
      * @param  int $id
      * @return \Illuminate\Http\Response
      */
-    public function update(Request $request, $id) {
-        //
+    public function update(Request $request, int $id) {
+        return response()->json(User::scopeUpdateUser($id, $request->all()));
     }
+
     /**
      * Remove the specified resource from storage.
      *
      * @param  int $id
      * @return \Illuminate\Http\Response
      */
-    public function destroy($id) {
-        //
+    public function destroy(int $id) {
+        return response()->json(User::scopeDestoryUser($id));
     }
 }
--- app/User.php
+++ app/User.php
@@ -6,6 +6,8 @@ use Illuminate\Notifications\Notifiable;
 use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 
+use Illuminate\Support\Facades\DB;
+
 class User extends Authenticatable
 {
     use Notifiable;
@@ -27,4 +29,57 @@ class User extends Authenticatable
     protected $hidden = [
         'password',
     ];
+
+    /**
+     * @return mixed
+     */
+    static public function scopeAllUsers() {
+        return self::whereNull('withdraw_at')->get();
+    }
+
+    /**
+     * see a user who been withdraw
+     *
+     * @param int $id
+     * @return mixed
+     */
+    static public function scopeGetUser(int $id) {
+        return self::where('id', $id)->first();
+    }
+
+    /**
+     * @param int $id
+     * @param array $datas
+     * @return array
+     */
+    static public function scopeUpdateUser(int $id, array $datas = []) {
+        $user = self::find($id);
+        try {
+            DB::beginTransaction();
+
+            foreach ($datas as $key => $data) {
+                $user->$key = $data;
+            }
+            $user->save();
+
+            DB::commit();
+        } catch (\Exception $e) {
+            DB::rollback();
+            return ['error' => $e->getMessage()];
+        }
+
+        return $user;
+    }
+
+    /**
+     * @param int $id
+     * @return array
+     */
+    static public function scopeDestoryUser(int $id) {
+        self::where('id', $id)->update([
+            'withdraw_at' => date('Y-m-d')
+        ]);
+
+        return ['message' => 'success'];
+    }
 }
