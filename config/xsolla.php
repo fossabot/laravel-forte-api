f6dfbc37a376f4c00aa99d899a6d4825e26977cf
Kevin <mingeun.k.k@gmail.com>
2019-03-07 00:50:40 +0900

    Added Xsolla Configuration

---
+++ config/xsolla.php
@@ -0,0 +1,8 @@
+<?php
+
+return [
+    'merchantId' => env('XSOLLA_MERCHANT_ID', ''),
+    'apiKey' => env('XSOLLA_API_KEY', ''),
+    'projectId' => env('XSOLLA_PROJECT_ID', ''),
+    'projectKey' => env('XSOLLA_PROJECT_KEY', ''),
+];
