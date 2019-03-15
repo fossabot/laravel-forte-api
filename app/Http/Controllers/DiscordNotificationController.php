8c5e666ee8bc5945fac5cd4557b4022410179f9d
Kevin <mingeun.k.k@gmail.com>
2019-03-16 02:09:33 +0900

    Added Error Tracking Discord Logging

---
+++ app/Http/Controllers/DiscordNotificationController.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use NotificationChannels\Discord\Discord;
+
+class DiscordNotificationController extends Controller
+{
+    /**
+     * @param \Exception $e
+     * @param array $data
+     * @return array
+     */
+    public function exception(\Exception $e, array $data = []) {
+        $params = json_encode($data, JSON_PRETTY_PRINT | JSON_UNESCAPED_SLASHES | JSON_UNESCAPED_UNICODE | JSON_NUMERIC_CHECK);
+
+        return app(Discord::class)->send('555413130872750091', [
+            'content' => '[' . config('app.env') . '> ' . now() . '] API ERROR',
+            'tts' => false,
+            'embed' => [
+                'title' => $e->getFile() . '(' . $e->getLine() . ')',
+                'description' => "`ERROR` \n {$e->getMessage()} \n `PARAMS` \n ``` {$params} ```"
+            ]
+        ]);
+    }
+}
