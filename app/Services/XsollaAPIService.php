5b629f86234513686901b735f196fe9cacab2504
Kevin <mingeun.k.k@gmail.com>
2019-03-19 00:48:01 +0900

    Added XsollaAPIService request API Module

---
+++ app/Services/XsollaAPIService.php
@@ -0,0 +1,80 @@
+<?php
+
+namespace App\Services;
+
+use GuzzleHttp\Client;
+use GuzzleHttp\Exception\GuzzleException;
+
+class XsollaAPIService
+{
+    /**
+     * @var Client
+     */
+    protected $client;
+    /**
+     * @var $merchantId
+     */
+    protected $merchantId;
+    /**
+     * @var $projectId
+     */
+    protected $projectId;
+    /**
+     * @var $projectKey
+     */
+    protected $projectKey;
+    /**
+     * @var $apiKey
+     */
+    protected $apiKey;
+    /**
+     * @var $authKey
+     */
+    protected $authKey;
+    /**
+     * @var $endpoint
+     */
+    protected $endpoint;
+
+    /**
+     * XsollaAPIService constructor.
+     * @param Client $client
+     */
+    public function __construct(Client $client) {
+        $this->client = $client;
+        $this->merchantId = config('xsolla.merchantId');
+        $this->projectId = config('xsolla.projectId');
+        $this->projectKey = config('xsolla.projectKey');
+        $this->apiKey = config('xsolla.apiKey');
+        $this->authKey = base64_encode($this->merchantId . ':' . $this->apiKey);
+        $this->endpoint = 'https://api.xsolla.com/merchant/v2/';
+    }
+
+    /**
+     * @param string $method
+     * @param string $uri
+     * @param array $datas
+     * @return mixed|\Psr\Http\Message\ResponseInterface
+     */
+    public function requestAPI(string $method, string $uri, array $datas) {
+        if (strpos($uri, ':projectId') !== false) {
+            $uri = str_replace(':projectId', $this->projectId, $uri);
+        } else {
+            $uri = str_replace(':merchantId', $this->merchantId, $uri);
+        }
+
+        try {
+            return $this->client->request($method, $this->endpoint . $uri, [
+                'form_params' => $datas,
+                'headers' => [
+                    'Authorization' => 'Basic ' . $this->authKey,
+                    'Accept' => 'application/json',
+                    'Content-Type' => 'application/json; charset=utf-8',
+                ],
+            ]);
+        } catch (GuzzleException $e) {
+            (new \App\Http\Controllers\DiscordNotificationController)->exception($e, $datas);
+            return $e;
+        }
+    }
+}
