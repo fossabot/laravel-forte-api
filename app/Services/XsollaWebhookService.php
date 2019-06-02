f7dba90d66e4ea498bd9a27243d59013911d02e8
Kevin <mingeun.k.k@gmail.com>
2019-06-02 16:02:33 +0900

    Test StyleCI

--- app/Services/XsollaWebhookService.php
+++ app/Services/XsollaWebhookService.php
@@ -162,8 +162,7 @@ class XsollaWebhookService
      * @return mixed
      * @see https://developers.xsolla.com/ko/api/v2/getting-started/#api_webhooks_refund
      */
-    public function refund(array $data)
-    {
+    public function refund(array $data) {
         return $data;
     }
 
