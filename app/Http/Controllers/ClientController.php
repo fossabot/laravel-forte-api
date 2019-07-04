424beb7b2ea4704e329c29c4c32d4e226bf2eb0b
Mingeun Kim <mingeun.k.k@gmail.com>
2019-07-05 02:42:37 +0900

    Modify Client Token Build Up

--- app/Http/Controllers/ClientController.php
+++ app/Http/Controllers/ClientController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use App\Client;
+use Illuminate\Http\JsonResponse;
 use Illuminate\Support\Facades\Hash;
 
 class ClientController extends Controller
@@ -15,7 +16,7 @@ class ClientController extends Controller
         foreach (Client::get() as $client) {
             if (! in_array($client->name, Client::BOT_TOKEN_RENEWAL_EXCEPTION)) {
                 Client::find($client->id)->update([
-                    'token' => 'forte-'.Hash::make(date('Y-m-d h:m:s')),
+                    'token' => 'forte-'.$this->generateToken(),
                     'prev_token' => $client->token,
                 ]);
             }
@@ -25,9 +26,23 @@ class ClientController extends Controller
     }
 
     /**
+     * @return string
+     */
+    private function generateToken() {
+        $merge = '';
+        $characters = '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ';
+
+        for ($i = 0; $i < 30; $i++) {
+            $merge .= $characters[rand(0, strlen($characters) - 1)];
+        }
+
+        return Hash::make($merge);
+    }
+
+    /**
      * 이전 토큰으로 현재 토큰을 조회합니다.
      *
-     * @return \Illuminate\Http\JsonResponse
+     * @return JsonResponse
      *
      * @SWG\Get(
      *     path="/clients/token",
