354e1e912e5d7079267127dd9afcd4f466df1d46
kang-heewon <itphantom@icloud.com>
2019-08-04 21:23:11 +0900

    Feature #65 - lara command add

--- bot.php
+++ bot.php
@@ -50,40 +50,50 @@ $forte = $discord->registerCommand('forte', function ($discord) {
     'description' => 'Forte Command List',
 ]);
 
-$forte->registerSubCommand('users', function ($discord) {
+$forte->registerSubCommand('users', function ($discord, $params) {
+    $userId = isset($params[0])?$params[0]:'';
     if (getenv('APP_ENV') === 'local') {
-        $users = exec('curl -X GET "http://localhost:8000/api/v1/users" -H "accept: application/json" -H "Authorization: '.getenv('DISCORD_LARA_TOKEN').'" -H "X-CSRF-TOKEN: "', $system);
+        $users = exec('curl -X GET "http://localhost:8000/api/v1/users/'.$userId.'" -H "accept: application/json" -H "Authorization: '.getenv('DISCORD_LARA_TOKEN').'" -H "X-CSRF-TOKEN: "', $system);
     } else {
-        $users = exec('curl -X GET "https://forte.team-crescendo.me/api/v1/users" -H "accept: application/json" -H "Authorization: '.getenv('DISCORD_LARA_TOKEN').'" -H "X-CSRF-TOKEN: "', $system);
+        $users = exec('curl -X GET "https://forte.team-crescendo.me/api/v1/users/'.$userId.'"-H "accept: application/json" -H "Authorization: '.getenv('DISCORD_LARA_TOKEN').'" -H "X-CSRF-TOKEN: "', $system);
     }
 
     $users = json_decode($users);
     $string = '';
 
-    foreach ($users as $index => $user) {
-        $index++;
-        $string .= $index.'. '.$user->name.' (ID: '.$user->id.' | EMAIL: '.$user->email.')'.PHP_EOL;
+    if($userId != ''){
+        $string .= $users->name.' (ID: '.$users->id.' | EMAIL: '.$users->email.')'.PHP_EOL;
+    }else{
+        foreach ($users as $index => $user) {
+            $index++;
+            $string .= $index.'. '.$user->name.' (ID: '.$user->id.' | EMAIL: '.$user->email.')'.PHP_EOL;
+        }
     }
-
     return $discord->reply('```'.$string.'```');
 }, [
     'description' => 'Forte Users',
 ]);
 
-$forte->registerSubCommand('items', function ($discord) {
+$forte->registerSubCommand('items', function ($discord, $params) {
+    $itemId = isset($params[0])?$params[0]:'';
     if (getenv('APP_ENV') === 'local') {
-        $items = exec('curl -X GET "http://localhost:8000/api/v1/items" -H "accept: application/json" -H "Authorization: '.getenv('DISCORD_LARA_TOKEN').'" -H "X-CSRF-TOKEN: "', $system);
+        $items = exec('curl -X GET "http://localhost:8000/api/v1/items/'.$itemId.'" -H "accept: application/json" -H "Authorization: '.getenv('DISCORD_LARA_TOKEN').'" -H "X-CSRF-TOKEN: "', $system);
     } else {
-        $items = exec('curl -X GET "https://forte.team-crescendo.me/api/v1/items" -H "accept: application/json" -H "Authorization: '.getenv('DISCORD_LARA_TOKEN').'" -H "X-CSRF-TOKEN: "', $system);
+        $items = exec('curl -X GET "https://forte.team-crescendo.me/api/v1/items/'.$itemId.'" -H "accept: application/json" -H "Authorization: '.getenv('DISCORD_LARA_TOKEN').'" -H "X-CSRF-TOKEN: "', $system);
     }
 
     $items = json_decode($items);
     $string = '';
 
-    foreach ($items as $index => $item) {
-        $index++;
-        $string .= $index.'. '.$item->name.' (ID: '.$item->id.' | '.number_format($item->price).' 원)'.PHP_EOL;
+    if($itemId != ''){
+        $string .= $items->name.' (ID: '.$items->id.' | '.number_format($items->price).' 원)'.PHP_EOL;
+    }else{
+        foreach ($items as $index => $item) {
+            $index++;
+            $string .= $index.'. '.$item->name.' (ID: '.$item->id.' | '.number_format($item->price).' 원)'.PHP_EOL;
+        }
     }
+
 
     return $discord->reply('```'.$string.'```');
 }, [
