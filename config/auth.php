ef4770cb8206a57a42e78d7ec847fe567b7f121d
kang-heewon <itphantom@icloud.com>
2019-08-05 22:45:52 +0900

    HOT FIX - login

--- config/auth.php
+++ config/auth.php
@@ -67,7 +67,7 @@ return [
     'providers' => [
         'users' => [
             'driver' => 'database',
-            'model' => App\Models\User::class,
+            'model' => 'users',
         ],
 
         // 'users' => [
