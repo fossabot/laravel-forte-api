a3e81fc4ccf207676c40a086042177fd12f21f19
kang-heewon <itphantom@icloud.com>
2019-12-27 03:00:37 +0900

    fix: 🔥 log copy command error

--- scripts/beforeInstall.bash
+++ scripts/beforeInstall.bash
@@ -8,6 +8,6 @@ if [ -d /var/www/release ]; then
     sudo rm -rf /var/www/release
 fi
 
-sudo cp /var/www/laravel-central-api/storage/backups /var/www/release-files
-sudo cp /var/www/laravel-central-api/storage/requests /var/www/release-files
+sudo cp -r /var/www/laravel-central-api/storage/backups /var/www/release-files
+sudo cp =r /var/www/laravel-central-api/storage/requests /var/www/release-files
 sudo mkdir -vp /var/www/release
