860bab307a4bd2d386fd0ae5ee12793e2ed321c5
kang-heewon <itphantom@icloud.com>
2019-12-27 03:09:34 +0900

    fix: 🔥️ typo

--- scripts/beforeInstall.bash
+++ scripts/beforeInstall.bash
@@ -9,5 +9,6 @@ if [ -d /var/www/release ]; then
 fi
 
 sudo cp -r /var/www/laravel-central-api/storage/backups /var/www/release-files
-sudo cp =r /var/www/laravel-central-api/storage/requests /var/www/release-files
+sudo cp -r /var/www/laravel-central-api/storage/requests /var/www/release-files
+sudo cp -r /var/www/laravel-central-api/storage/logs /var/www/release-files
 sudo mkdir -vp /var/www/release
