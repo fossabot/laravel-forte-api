0e98c3c79b200b9b9bcbd2447ce1edf698425424
Mingeun <mingeun.k.k@gmail.com>
2020-04-30 12:27:48 +0900

    Delete dashboard and mail backup controller

--- routes/web.php
+++ routes/web.php
@@ -19,28 +19,11 @@ Route::get('error', function () {
     return view('xsolla.error');
 });
 
-// disable inventory and shops
-//Route::get('inventory', 'UserItemController@inventory')->name('item.inventory')->middleware('auth.forte');
-//Route::get('shops/{token}', 'UserController@shortXsollaURL')->name('xsolla.short')->middleware('auth.forte');
 Route::get('panel', function () {
     return \Socialite::with('discord')->redirect();
 });
 Route::get('panel/{token}', 'UserController@panel')->name('user.panel')->middleware('auth.forte');
 Route::post('withdraw', 'UserItemController@withdraw');
-
-/*
- * @deprecated admin dashboard
- */
-Route::prefix('dashboard/')->group(function () {
-    Route::any('/signin', 'DashboardController@signin');
-    Route::middleware('auth')->group(function () {
-        Route::get('/', 'DashboardController@index')->name('dashboard.index');
-        Route::get('/users', 'DashboardController@users')->name('dashboard.users');
-        Route::get('/errors', 'DashboardController@errors')->name('dashboard.errors');
-
-        Route::post('logout', 'DashboardController@logout')->name('dashboard.logout');
-    });
-});
 
 Route::get('login/discord', function () {
     return \Socialite::with('discord')->redirect();
