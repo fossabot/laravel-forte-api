953a976e6b1891d4a089f8f0d4c78d24e250ec39
Mingeun Kim <mingeun.k.k@gmail.com>
2020-05-17 00:18:51 +0900

    Added Enums

---
+++ app/Enums/ItemWithdrawDisable.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Enums;
+
+use MadWeb\Enum\Enum;
+
+final class ItemWithdrawDisable extends Enum
+{
+    const SBK_5 = 'skb_5';
+    const SKB_9 = 'skb_9';
+    const SKB_12 = 'skb_12';
+}
