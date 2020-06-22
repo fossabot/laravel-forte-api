b091d46042f823601ea069690acef1349d062484
Mingeun Kim <mingeun.k.k@gmail.com>
2020-06-23 01:30:34 +0900

    feat(migration): #128 migration table

---
+++ database/migrations/2020_06_23_001108_create_v2_attendances_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateV2AttendancesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('v2_attendances', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->string('discord_id');
+            $table->integer('key_count')->default(1)->comment('키 획득 count');
+            $table->json('key_acquired_at')->comment('키 획득 일');
+            $table->timestamps();
+
+            $table->foreign('discord_id')
+                ->references('discord_id')->on('users')
+                ->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('v2_attendances');
+    }
+}
