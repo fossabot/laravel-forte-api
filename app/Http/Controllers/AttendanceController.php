b48bcf469203a40a62eb05029213c8dab4151cd8
Mingeun Kim <mingeun.k.k@gmail.com>
2020-06-25 22:56:50 +0900

    fix(controller): #128 store method bug fix

--- app/Http/Controllers/AttendanceController.php
+++ app/Http/Controllers/AttendanceController.php
@@ -68,13 +68,6 @@ class AttendanceController extends Controller
      *         required=true,
      *         type="string"
      *     ),
-     *     @SWG\Parameter(
-     *         name="isPremium",
-     *         in="query",
-     *         description="User Premium Role Check",
-     *         required=true,
-     *         type="integer"
-     *     ),
      *     @SWG\Response(
      *         response=201,
      *         description="Successful User Attendance v2"
@@ -88,18 +81,18 @@ class AttendanceController extends Controller
             ->whereDiscordId($id)
             ->first();
 
-        if ($attendance->isEmpty()) {
-            $attendance = AttendanceV2::insert([
+        if (! $attendance) {
+            AttendanceV2::insert([
                 AttendanceV2::DISCORD_ID => $id,
                 AttendanceV2::KEY_ACQUIRED_AT => json_encode([$date]),
             ]);
 
             return new JsonResponse([
                 'status' => 'success',
-                AttendanceV2::KEY_COUNT => $attendance->{AttendanceV2::KEY_COUNT},
+                AttendanceV2::KEY_COUNT => 1,
             ], Response::HTTP_CREATED);
         } else {
-            $keyAcquiredAt = $attendance->key_acquired_at;
+            $keyAcquiredAt = collect(json_decode($attendance->key_acquired_at, true));
 
             if ($keyAcquiredAt->last() && Carbon::parse($keyAcquiredAt->last())->isToday()) {
                 $timeDiff = Carbon::now()
@@ -112,7 +105,7 @@ class AttendanceController extends Controller
                 ], Response::HTTP_CONFLICT);
             }
 
-            if ($attendance->key_count <= self::KEY_MAX_COUNT) {
+            if ($attendance->key_count < self::KEY_MAX_COUNT) {
                 $attendance->update([
                     AttendanceV2::KEY_COUNT => $attendance->key_count + 1,
                     AttendanceV2::KEY_ACQUIRED_AT => $keyAcquiredAt->push($date),
