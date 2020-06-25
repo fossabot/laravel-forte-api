6aba3fcc88310bdc0f21bf5cfa1ccf0cff7ca514
Mingeun Kim <mingeun.k.k@gmail.com>
2020-06-26 02:05:47 +0900

    feat(enum): 출석체크 박스 등급에 대한 열거형 클래스 추가

---
+++ app/Enums/AttendanceBoxType.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Enums;
+
+use MadWeb\Enum\Enum;
+
+final class AttendanceBoxType extends Enum
+{
+    const BRONZE = 'bronze';
+    const SILVER = 'silver';
+    const GOLD = 'gold';
+}
