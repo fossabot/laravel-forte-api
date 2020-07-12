3a75fbb6365ac137b16a7c3cb563cd136b9ee84b
Mingeun Kim <mingeun.k.k@gmail.com>
2020-07-12 22:55:51 +0900

    feat: 라라벨 버전 업으로 인한 Foundation\Auth 미지원 수정

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -4,7 +4,6 @@ namespace App\Http\Controllers\Auth;
 
 use App\Http\Controllers\Controller;
 use App\User;
-use Illuminate\Foundation\Auth\RegistersUsers;
 use Illuminate\Support\Facades\Hash;
 use Illuminate\Support\Facades\Validator;
 
@@ -20,8 +19,6 @@ class RegisterController extends Controller
     | provide this functionality without requiring any additional code.
     |
     */
-
-    use RegistersUsers;
 
     /**
      * Where to redirect users after registration.
