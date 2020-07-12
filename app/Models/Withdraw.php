aee9a2a5601c24a6ea5efd112a07a75b7904a762
Mingeun Kim <mingeun.k.k@gmail.com>
2020-07-12 22:54:21 +0900

    feat(inspection): 청약철회 모델 inspection

--- app/Models/Withdraw.php
+++ app/Models/Withdraw.php
@@ -4,6 +4,26 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
 
+/**
+ * App\Models\Withdraw
+ *
+ * @property int $id
+ * @property int $item_id
+ * @property int $user_id
+ * @property \Illuminate\Support\Carbon|null $created_at
+ * @property \Illuminate\Support\Carbon|null $updated_at
+ * @property int|null $user_item_id
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Models\Withdraw newModelQuery()
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Models\Withdraw newQuery()
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Models\Withdraw query()
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Models\Withdraw whereCreatedAt($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Models\Withdraw whereId($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Models\Withdraw whereItemId($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Models\Withdraw whereUpdatedAt($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Models\Withdraw whereUserId($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Models\Withdraw whereUserItemId($value)
+ * @mixin \Eloquent
+ */
 class Withdraw extends Model
 {
     const ID = 'id';
