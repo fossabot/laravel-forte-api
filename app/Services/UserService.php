6e396d888ee3afbb8e7230cfe02f1f90edd8b9a1
Mingeun Kim <mingeun.k.k@gmail.com>
2020-07-18 15:17:37 +0900

    refactor: user service layer return type hint

--- app/Services/UserService.php
+++ app/Services/UserService.php
@@ -37,18 +37,21 @@ class UserService extends BaseService
         $this->xsollaAPI = $xsollaAPIService;
     }
 
+    /**
+     * @return Paginator
+     */
     public function index(): Paginator
     {
-        return $this->user->whereNull($this->user::DELETED_AT)->paginate();
+        return $this->user->whereNull(User::DELETED_AT)->paginate();
     }
 
     /**
      * @param int $id
-     * @return User|User[]|Collection|Model|null
+     * @return User
      */
     public function show(int $id): User
     {
-        return $this->user->find($id);
+        return $this->user->findOrFail($id);
     }
 
     /**
@@ -122,9 +125,9 @@ class UserService extends BaseService
 
     /**
      * @param int $id
-     * @return User|Builder|Model|object
+     * @return User|Builder
      */
-    public function discord(int $id): User
+    public function discord(int $id)
     {
         return $this->user->where($this->user::DISCORD_ID, $id)->firstOrFail();
     }
