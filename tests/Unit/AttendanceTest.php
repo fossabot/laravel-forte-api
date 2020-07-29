f45910897d4422504e3e6acd4e1c0bc85d6ddb2e
HyungJu <sungkisa@naver.com>
2020-07-29 09:27:21 +0900

    test: create test for Attendance #177

---
+++ tests/Unit/AttendanceTest.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace Tests\Unit;
+
+use PHPUnit\Framework\TestCase;
+
+class AttendanceTest extends TestCase
+{
+    /**
+     * A basic unit test example.
+     *
+     * @return void
+     */
+    public function testExample()
+    {
+        $this->assertTrue(true);
+    }
+
+    public function testCanBuildPointByProbability()
+    {
+
+    }
+
+    public function testCanCheckValidateBoxFromKeyCount()
+    {
+
+    }
+
+    public function testCanBuildProbabilityBoxPackage()
+    {
+        
+    }
+}
