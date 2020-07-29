989dfde856b84004b7003fda8e91308182476996
HyungJu <sungkisa@naver.com>
2020-07-29 09:27:21 +0900

    test: create test for Attendance

---
+++ tests/Feature/AttendanceTest.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace Tests\Feature;
+
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Tests\TestCase;
+
+class AttendanceTest extends TestCase
+{
+    public function testCanQueryUserKeyAmount()
+    {
+
+    }
+
+    public function testCanCheckAttendance()
+    {
+
+    }
+
+    public function testCanOpenBox()
+    {
+
+    }
+}
